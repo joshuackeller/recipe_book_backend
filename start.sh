a28f1ccf22191eadc57b37a67343334818b340d6
joshuackeller <joshuackeller@gmail.com>
2023-07-29 23:02:31 -0600

    change file ownership

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker build -t recipes_images .
-docker run -p 80:80 --name recipes_container -d recipes_images
+docker build -t recipes_image .
+docker run -p 80:80 --name recipes_container -d recipes_image
