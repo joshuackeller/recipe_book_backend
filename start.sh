a6852d9dfeb9f120c4c5c4573f0fe31dbc8bb1af
joshuackeller <joshuackeller@gmail.com>
2023-07-29 22:54:46 -0600

    add bash scripts

---
+++ start.sh
@@ -0,0 +1,2 @@
+docker build -t recipes_images .
+docker run -p 80:80 --name recipes_container -d recipes
---
+++ terminate.sh
@@ -0,0 +1,3 @@
+docker stop recipes_container
+docker rm recipes_container
+docker rmi recipes_images
