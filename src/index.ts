ba35b7e07a422af816141bb73437951d5faab064
joshuackeller <joshuackeller@gmail.com>
2023-08-04 10:48:54 -0600

    try cicd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import auth from "./routes/auth";
-import { createServer } from "node:https";
-import fs from "node:fs";
 import recipes from "./routes/recipes";
 
 const app = new Hono();
+
+app.get("/", (c) => c.text("hello there"));
 
 app.route("/auth", auth);
 app.route("/recipes", recipes);
@@ -20,16 +20,7 @@ app.onError((err, c) => {
 serve({
   fetch: app.fetch,
   port: 4500,
-  // hostname: "recipes-api.joshkeller.info"
-  // serverOptions: prod ?
-  //   {
-  //     key: fs.readFileSync('../keys/agent1-key.pem'),
-  //     cert: fs.readFileSync('../keys/agent1-cert.pem'),
-  //   } : undefined,
-  //   hostname
-  // createServer
-  // https://github.com/honojs/node-server
-  // https://dev.to/shadid12/how-to-deploy-your-node-js-app-on-aws-with-nginx-and-ssl-3p5l
+  // https://infisical.com/
 });
 
 console.log(`Running on http://localhost:${process.env.PORT ?? 4500}`);
